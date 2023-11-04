9a1f611b2c6c131f19d488c6a92a224d7ff96e7c
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-11-04 12:09:21 +0300

    wrap up

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 const Page = () => {
-  return <div>Customer Page</div>;
+  return <div>Customers Page</div>;
 };
 
 export default Page;
